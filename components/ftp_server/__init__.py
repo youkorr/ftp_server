3021d3dd25c4b3b69b49703b59899c99c90af162
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-21 00:32:37 +0100

    Update __init__.py

--- components/ftp_server/__init__.py
+++ components/ftp_server/__init__.py
@@ -2,12 +2,15 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.const import CONF_ID, CONF_PASSWORD, CONF_USERNAME, CONF_PORT
 
-DEPENDENCIES = ['network']
+# Importer la configuration de la carte SD
+from esphome.components import sd_mmc_card
+
+DEPENDENCIES = ['network', 'sd_mmc_card']
 CODEOWNERS = ['@votre_nom']
 
 # Définir les constantes pour la configuration
 CONF_ROOT_PATH = 'root_path'
-
+CONF_SD_CARD = 'sd_card'
 
 # Créer l'espace de noms et la classe FTP
 ftp_ns = cg.esphome_ns.namespace('ftp_server')
@@ -18,8 +21,11 @@ CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(FTPServer),
     cv.Required(CONF_USERNAME): cv.string,
     cv.Required(CONF_PASSWORD): cv.string,
-    cv.Required(CONF_ROOT_PATH): cv.string,
+    cv.Optional(CONF_ROOT_PATH, default='/sdcard'): cv.string,
     cv.Optional(CONF_PORT, default=21): cv.port,
+    
+    # Ajout de la configuration de la carte SD
+    cv.Required(CONF_SD_CARD): cv.use_id(sd_mmc_card.SDMMCCard)
 }).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
@@ -31,3 +37,8 @@ async def to_code(config):
     cg.add(var.set_password(config[CONF_PASSWORD]))
     cg.add(var.set_root_path(config[CONF_ROOT_PATH]))
     cg.add(var.set_port(config[CONF_PORT]))
+    
+    # Lier la carte SD au serveur FTP
+    sd_card = await cg.get_variable(config[CONF_SD_CARD])
+    cg.add(var.set_sd_mmc_card(sd_card))
+
