1ed6b0c5a0fa03f0637f84ab570ea652664f38fd
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-20 02:00:07 +0100

    Create ftp_server.h

---
+++ components/ftp_server/ftp_server.h
@@ -0,0 +1,90 @@
+#pragma once
+
+#include "esphome/core/component.h"
+#include "esphome/core/automation.h"
+#include <string>
+#include <WiFiServer.h>
+#include <WiFiClient.h>
+#include <SD.h>
+#include <FS.h>
+#include <vector>
+
+namespace esphome {
+namespace ftp_server {
+
+class FTPServer : public Component {
+ public:
+  FTPServer();
+  
+  void setup() override;
+  void loop() override;
+  void dump_config() override;
+  float get_setup_priority() const override { return setup_priority::AFTER_WIFI; }
+  
+  void set_username(std::string username) { username_ = username; }
+  void set_password(std::string password) { password_ = password; }
+  void set_root_path(std::string root_path) { root_path_ = root_path; }
+  void set_port(uint16_t port) { port_ = port; }
+  
+ protected:
+  enum FtpState {
+    FTP_IDLE,
+    FTP_WAIT_LOGIN,
+    FTP_WAIT_PASSWORD,
+    FTP_LOGGED_IN,
+    FTP_WAIT_COMMAND,
+    FTP_TRANSFER
+  };
+  
+  enum TransferMode {
+    NONE,
+    PASSIVE,
+    ACTIVE
+  };
+  
+  void handle_new_clients();
+  void handle_ftp_client(WiFiClient& client);
+  void process_command(WiFiClient& client, const std::string& command);
+  void send_response(WiFiClient& client, int code, const std::string& message);
+  
+  bool authenticate(const std::string& username, const std::string& password);
+  void start_data_connection();
+  void stop_data_connection();
+  bool open_file(const std::string& path, const char* mode);
+  void close_file();
+  void list_directory(WiFiClient& data_client, const std::string& path);
+  void send_file_data(WiFiClient& data_client);
+  void receive_file_data(WiFiClient& data_client);
+  
+  std::string username_;
+  std::string password_;
+  std::string root_path_;
+  uint16_t port_;
+  
+  WiFiServer* ftp_server_{nullptr};
+  WiFiServer* data_server_{nullptr};
+  WiFiClient data_client_;
+  
+  FtpState state_{FTP_IDLE};
+  TransferMode transfer_mode_{NONE};
+  
+  uint16_t data_port_{0};
+  std::string data_ip_;
+  
+  std::string current_path_;
+  std::string rename_from_;
+  
+  File current_file_;
+  
+  // Buffer pour économiser la mémoire
+  std::vector<char> buffer_;
+  
+  // Informations de connexion client
+  std::vector<WiFiClient> clients_;
+  std::vector<FtpState> client_states_;
+  std::vector<std::string> client_usernames_;
+  std::vector<std::string> client_current_paths_;
+};
+
+}  // namespace ftp_server
+}  // namespace esphome
