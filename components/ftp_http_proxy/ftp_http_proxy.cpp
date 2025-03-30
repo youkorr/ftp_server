2f97fb5f8a196909cecb136ed76ea5ca10df5113
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-30 05:03:06 +0200

    Update ftp_http_proxy.cpp

--- components/ftp_http_proxy/ftp_http_proxy.cpp
+++ components/ftp_http_proxy/ftp_http_proxy.cpp
@@ -87,10 +87,11 @@ bool FTPHTTPProxy::download_file(const std::string &remote_path, httpd_req_t *re
   char *pasv_start = nullptr;
   int data_port = 0;
   int ip[4], port[2]; 
+  char buffer[1024]; // Tampon de 1ko pour réception
   int bytes_received;
   int flag = 1;  // Déplacé avant les goto
   int rcvbuf = 8192; // Déplacé avant les goto
-  
+
   // Connexion au serveur FTP
   if (!connect_to_ftp()) {
     ESP_LOGE(TAG, "Échec de connexion FTP");
