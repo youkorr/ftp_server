5720dd4e0a5ebad84f0e5d4eb1fac34d28e2470b
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-30 19:35:58 +0200

    Update sd_mmc_card.h

--- components/sd_mmc_card/sd_mmc_card.h
+++ components/sd_mmc_card/sd_mmc_card.h
@@ -5,13 +5,14 @@
 #include <functional>
 #include "esphome/core/component.h"
 #include "esphome/core/gpio.h"
-
-#ifdef USE_TEXT_SENSOR
-#include "esphome/components/text_sensor/text_sensor.h"
-#endif
+#include "esphome/core/defines.h"
 
 #ifdef USE_SENSOR
 #include "esphome/components/sensor/sensor.h"
+#endif
+
+#ifdef USE_TEXT_SENSOR
+#include "esphome/components/text_sensor/text_sensor.h"
 #endif
 
 namespace esphome {
@@ -49,6 +50,7 @@ class SdMmc : public Component {
   void setup() override;
   void loop() override;
   void dump_config() override;
+  float get_setup_priority() const override { return setup_priority::DATA; }
 
   void set_clk_pin(int pin) { this->clk_pin_ = pin; }
   void set_cmd_pin(int pin) { this->cmd_pin_ = pin; }
@@ -57,10 +59,44 @@ class SdMmc : public Component {
   void set_data2_pin(int pin) { this->data2_pin_ = pin; }
   void set_data3_pin(int pin) { this->data3_pin_ = pin; }
   void set_power_ctrl_pin(GPIOPin *pin) { this->power_ctrl_pin_ = pin; }
+  void set_mode_1bit(bool mode) { this->mode_1bit_ = mode; }
 
+  void write_file_chunked(const char *path, const uint8_t *buffer, size_t len, const char *mode);
+  void write_file(const char *path, const uint8_t *buffer, size_t len);
+  void write_file(const char *path, const uint8_t *buffer, size_t len, const char *mode);
+  void append_file(const char *path, const uint8_t *buffer, size_t len);
+  std::vector<uint8_t> read_file(const char *path);
+  void read_file_chunked(const char *path, std::function<bool(const uint8_t *, size_t)> callback);
+  bool process_file(const char *path, std::function<bool(const uint8_t *, size_t)> callback, size_t buffer_size);
+  bool copy_file(const char *source_path, const char *dest_path);
+  bool delete_file(const char *path);
+  bool create_directory(const char *path);
+  bool remove_directory(const char *path);
+  bool file_exists(const char *path);
+  size_t file_size(const char *path);
+  bool is_directory(const char *path);
+  std::vector<std::string> list_directory(const char *path, uint8_t depth);
+  std::vector<FileInfo> list_directory_file_info(const char *path, uint8_t depth);
+
+#ifdef USE_SENSOR
+  void set_used_space_sensor(sensor::Sensor *sens) { this->used_space_sensor_ = sens; }
+  void set_total_space_sensor(sensor::Sensor *sens) { this->total_space_sensor_ = sens; }
+  void set_free_space_sensor(sensor::Sensor *sens) { this->free_space_sensor_ = sens; }
+  void add_file_size_sensor(sensor::Sensor *sensor, std::string const &path);
   void set_memory_unit(MemoryUnits unit) { this->memory_unit_ = unit; }
+#endif
+
+#ifdef USE_TEXT_SENSOR
+  void set_sd_card_type_text_sensor(text_sensor::TextSensor *sens) { this->sd_card_type_text_sensor_ = sens; }
+#endif
 
  protected:
+  void unmount();
+  void update_sensors();
+  void list_directory_file_info_rec(const char *path, uint8_t depth, std::vector<FileInfo> &list);
+
+  std::string error_code_to_string(ErrorCode code);
+
   bool mode_1bit_{false};
   bool mounted_{false};
   void *card_{nullptr};
@@ -83,7 +119,7 @@ class SdMmc : public Component {
 #endif
 
 #ifdef USE_TEXT_SENSOR
-  text_sensor::TextSensor *sd_card_type_{nullptr};
+  text_sensor::TextSensor *sd_card_type_text_sensor_{nullptr};
 #endif
 };
 
