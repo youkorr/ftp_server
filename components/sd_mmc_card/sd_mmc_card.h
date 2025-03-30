c9b3d2cb8d9b64ee5bcd8f94d6b9b4d1b8fa1315
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-30 19:02:31 +0200

    Update sd_mmc_card.h

--- components/sd_mmc_card/sd_mmc_card.h
+++ components/sd_mmc_card/sd_mmc_card.h
@@ -68,49 +68,10 @@ class SdMmc : public Component {
   void dump_config() override;
   float get_setup_priority() const override { return setup_priority::DATA; }
 
-  // File operations
-  void write_file(const char *path, const uint8_t *buffer, size_t len);
-  void write_file(const char *path, const uint8_t *buffer, size_t len, const char *mode);
-  void append_file(const char *path, const uint8_t *buffer, size_t len);
-  
-  std::vector<uint8_t> read_file(const char *path);
-  std::vector<uint8_t> read_file(std::string const &path) {
-    return this->read_file(path.c_str());
-  }
-  
-  bool delete_file(const char *path);
-  bool delete_file(std::string const &path) {
-    return this->delete_file(path.c_str());
-  }
+#ifdef USE_SENSOR
+  void update_sensors(); // Ajout de la déclaration manquante.
+#endif
 
-  // Chunked operations
-  void write_file_chunked(const char *path, const uint8_t *buffer, size_t len, const char *mode);
-  void read_file_chunked(const char *path, std::function<bool(const uint8_t*, size_t)> callback);
-  
-  bool process_file(const char *path, std::function<bool(const uint8_t*, size_t)> callback, size_t buffer_size = 4096);
-  bool process_file(std::string const &path, std::function<bool(const uint8_t*, size_t)> callback, size_t buffer_size = 4096) {
-    return this->process_file(path.c_str(), callback, buffer_size);
-  }
-
-  // File management
-  bool copy_file(const char *source_path, const char *dest_path);
-  bool copy_file(std::string const &source_path, std::string const &dest_path) {
-    return this->copy_file(source_path.c_str(), dest_path.c_str());
-  }
-
-  // Directory operations
-  bool create_directory(const char *path);
-  bool remove_directory(const char *path);
-  
-  std::vector<std::string> list_directory(const char *path, uint8_t depth = 0);
-  std::vector<FileInfo> list_directory_file_info(const char *path, uint8_t depth = 0);
-  
-  // File info
-  size_t file_size(const char *path);
-  bool file_exists(const char *path);
-  bool is_directory(const char *path);
-
-  // Sensors
 #ifdef USE_SENSOR
   void set_used_space_sensor(sensor::Sensor *sens) { this->used_space_sensor_ = sens; }
   void set_total_space_sensor(sensor::Sensor *sens) { this->total_space_sensor_ = sens; }
@@ -123,49 +84,14 @@ class SdMmc : public Component {
   void set_sd_card_type_text_sensor(text_sensor::TextSensor *sens) { this->sd_card_type_text_sensor_ = sens; }
 #endif
 
-  // Pin configuration
-  void set_clk_pin(uint8_t pin) { this->clk_pin_ = pin; }
-  void set_cmd_pin(uint8_t pin) { this->cmd_pin_ = pin; }
-  void set_data0_pin(uint8_t pin) { this->data0_pin_ = pin; }
-  void set_data1_pin(uint8_t pin) { this->data1_pin_ = pin; }
-  void set_data2_pin(uint8_t pin) { this->data2_pin_ = pin; }
-  void set_data3_pin(uint8_t pin) { this->data3_pin_ = pin; }
-  void set_mode_1bit(bool b) { this->mode_1bit_ = b; }
-  void set_power_ctrl_pin(GPIOPin *pin) { this->power_ctrl_pin_ = pin; }
-
-  // Status
-  enum class ErrorCode {
-    NONE = 0,
-    ERR_PIN_SETUP,
-    ERR_MOUNT,
-    ERR_NO_CARD,
-  };
-  
-  bool is_failed() const { return this->init_error_ != ErrorCode::NONE; }
-  static std::string error_code_to_string(ErrorCode code);
-
  protected:
   void list_directory_file_info_rec(const char *path, uint8_t depth, std::vector<FileInfo> &list);
-  void unmount();
-  
-  uint8_t clk_pin_;
-  uint8_t cmd_pin_;
-  uint8_t data0_pin_;
-  uint8_t data1_pin_;
-  uint8_t data2_pin_;
-  uint8_t data3_pin_;
-  bool mode_1bit_;
-  bool mounted_;
-  GPIOPin *power_ctrl_pin_{nullptr};
-  void *card_{nullptr};  // sdmmc_card_t*
-  ErrorCode init_error_{ErrorCode::NONE};
   
 #ifdef USE_SENSOR
   sensor::Sensor *used_space_sensor_{nullptr};
   sensor::Sensor *total_space_sensor_{nullptr};
   sensor::Sensor *free_space_sensor_{nullptr};
   std::vector<FileSizeSensor> file_size_sensors_;
-  MemoryUnits memory_unit_{MemoryUnits::MEGABYTES};
 #endif
   
 #ifdef USE_TEXT_SENSOR
@@ -173,5 +99,6 @@ class SdMmc : public Component {
 #endif
 };
 
-}  // namespace sd_mmc_card
-}  // namespace esphome
+} // namespace sd_mmc_card
+} // namespace esphome
+
